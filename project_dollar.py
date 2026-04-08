2fab94c10cbf01ce0b3c869f5ad0b24476840a8b
diogompc3 <89374558+diogompc3@users.noreply.github.com>
2026-04-08 15:49:41 -0300

    Add files via upload

---
+++ project_dollar.py
@@ -0,0 +1,23 @@
+#Bibliotecas em uso
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+#Baixando os dados e os direcionando para uma variável
+print("Baixando os dados...")
+data_dollar = yf.download('BRL=X', period='2y')
+
+#Primeiras linhas dos dados
+print('\nPrimeiras linhas dos dados: ')
+print(data_dollar.head())
+
+#Criando e configurando o gráfico
+plt.figure(figsize=(10, 5))
+plt.plot(data_dollar.index, data_dollar['Close'], color='green')
+plt.title('Variação da Cotação do Dólar (BRL) - 2024-2026')
+plt.xlabel('Data')
+plt.ylabel('Valor do Dólar (R$)')
+plt.grid(True)
+
+#Exibição do gráfico
+plt.show()
